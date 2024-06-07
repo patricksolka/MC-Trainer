b6ac238c995d1d0389218e0aebf413c3d198296d
Patrick <patrick.solka@mni.thm.de>
2024-06-07 12:40:19 +0200

    changed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { StatsComponent } from './components/stats/stats.component';
+import {AchievementsComponent} from "./components/achievements/achievements.component";
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'homeComponent',
-    loadComponent: () => import('./components/home/home.component').then( m => m.HomeComponent)
-  }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'cards', component: CardComponent },
+  { path: 'stats', component: StatsComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: '**', redirectTo: 'home' },
 ];
-
