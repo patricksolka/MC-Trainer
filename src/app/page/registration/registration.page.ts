c8a36be6cedd8452a6c8fae2e06174c94eb295c3
Dimitrios Paliouras <dimitrios.paliouras@mni.thm.de>
2024-06-07 18:49:43 +0200

    Add login/registration but not finished yet - needs to be revised

--- src/app/page/registration/registration.page.ts
+++ src/app/page/registration/registration.page.ts
@@ -1,20 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonBackButton, IonButton,
+  IonButtons, IonCol,
+  IonContent,
+  IonHeader, IonInput,
+  IonItem, IonLabel, IonList, IonRouterLink, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton, IonList, IonRouterLink, RouterLink]
 })
-export class RegistrationPage implements OnInit {
+export class RegistrationPage {
+  username: string = '';
+  password: string = '';
+  email: string = '';
+  firstname: string = '';
+  lastname: string = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  register() {
+    const user = new User(this.username, this.password, this.email, this.firstname, this.lastname);
+    if (this.authService.register(user)) {
+      this.router.navigateByUrl('/login');
+    } else {
+      alert('Username already exists');
+    }
   }
-
 }
