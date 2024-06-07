b75c9569ec052a5af87310ea8b3aacca6f1d0aef
Dimitrios Paliouras <dimitrios.paliouras@mni.thm.de>
2024-06-07 18:49:43 +0200

    Add login/registration but not finished yet - needs to be revised

--- src/app/page/forgot-password/forgot-password.page.ts
+++ src/app/page/forgot-password/forgot-password.page.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonBackButton, IonButton,
+  IonButtons, IonCol,
+  IonContent,
+  IonHeader, IonInput,
+  IonItem, IonLabel, IonList, IonRow,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { AuthService } from '../../services/auth.service';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton, IonList, RouterLink]
 })
-export class ForgotPasswordPage implements OnInit {
+export class ForgotPasswordPage {
+  username: string = '';
+  newPassword: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService) { }
+  resetPassword() {
+    if (this.authService.resetPassword(this.username, this.newPassword)) {
+      alert('Password reset successfully');
+    } else {
+      alert('Username not found');
+    }
   }
-
 }
