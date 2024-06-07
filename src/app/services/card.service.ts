225de2044d35bea02d268aff01822218708a4b1b
Patrick <patrick.solka@mni.thm.de>
2024-06-07 12:46:11 +0200

    added "services"-folder

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Question } from '../../models/question.model';
+import { Question } from '../models/question.model';
 
 @Injectable({
   providedIn: 'root'
