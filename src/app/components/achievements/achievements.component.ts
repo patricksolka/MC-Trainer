d04c4c3e6768425758fa13c89fdaa2e496f170a4
Patrick <patrick.solka@mni.thm.de>
2024-06-07 12:48:21 +0200

    changed folder structure

--- src/app/components/achievements/achievements.component.ts
+++ src/app/components/achievements/achievements.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {AchievementService} from "./achievement.service";
+import {AchievementService} from "../../services/achievement.service";
 
 @Component({
     selector: 'app-achievements',
