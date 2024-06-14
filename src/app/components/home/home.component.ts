4f1c295eec0a88788cf558d30f042bfef9908ab8
Patrick <patrick.solka@mni.thm.de>
2024-06-14 20:33:00 +0200

    added logoutButton for Testing (buggy)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import {IonicModule, LoadingController} from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,16 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, RouterModule]
 })
-export class HomeComponent { }
+export class HomeComponent {
+  constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {
+  }
+  async logout() {
+    const loading = await this.loadingController.create({
+      message: 'Logging out...',
+    });
+    await loading.present();
+    await this.authService.logout();
+    loading.dismiss();
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
+}
