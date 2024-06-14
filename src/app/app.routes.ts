686a9ece63505f14694631495ae36ffe8947cfa9
Patrick <patrick.solka@mni.thm.de>
2024-06-14 17:40:27 +0200

    implemented authGuard for routes allow access to main pages only if authorized; otherwise redirect to login oder register (or onboarding)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from "@angular/fire/auth-guard";
+
+const redirectUnauthorizedToLogin = () => {
+  return redirectUnauthorizedTo(['login']);
+};
+
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 export const routes: Routes = [
   {
@@ -7,32 +14,38 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'login',
+    loadComponent: () => import('./components/auth/login/login.page').then(m => m.LoginPage),
+    ...canActivate(redirectLoggedInToHome)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./components/auth/registration/registration.page').then(m => m.RegistrationPage),
+    ...canActivate(redirectLoggedInToHome)
+  },
+  {
     path: 'home',
-    loadComponent: () => import('./components/home/home.component').then( m => m.HomeComponent)
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'cards',
-    loadComponent: () => import('./components/card/card.component').then( m => m.CardComponent)
+    loadComponent: () => import('./components/card/card.component').then(m => m.CardComponent),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'stats',
-    loadComponent: () => import('./components/stats/stats.component').then( m => m.StatsComponent)
+    loadComponent: () => import('./components/stats/stats.component').then(m => m.StatsComponent),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'achievements',
-    loadComponent: () => import('./components/achievements/achievements.component').then( m => m.AchievementsComponent)
+    loadComponent: () => import('./components/achievements/achievements.component').then(m => m.AchievementsComponent),
+    ...canActivate(redirectUnauthorizedToLogin)
   },
   {
     path: 'forgot-password',
     loadComponent: () => import('./components/auth/forgot-password/forgot-password.page').then(m => m.ForgotPasswordPage)
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./components/auth/login/login.page').then(m => m.LoginPage)
-  },
-  {
-    path: 'register',
-    loadComponent: () => import('./components/auth/registration/registration.page').then(m => m.RegistrationPage)
   },
 
 ];
