1283954981ce6ed35ec28cd5724e4262753665b4
Patrick <patrick.solka@mni.thm.de>
2024-06-15 12:34:07 +0200

    added cardService into constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,55 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {IonApp, IonRouterOutlet} from "@ionic/angular/standalone";
 import {addIcons} from "ionicons";
-import {personOutline, chevronForward,lockClosedOutline,mailOutline, home, book, addCircle, statsChart, person, arrowBackOutline, logoGoogle} from "ionicons/icons";
+import {
+    personOutline,
+    chevronForward,
+    lockClosedOutline,
+    mailOutline,
+    home,
+    book,
+    addCircle,
+    statsChart,
+    person,
+    arrowBackOutline,
+    logoGoogle
+} from "ionicons/icons";
 import {Router} from "@angular/router";
-import { HttpClientModule } from '@angular/common/http';
-import { CardService } from './services/card.service';
-import { Question } from './models/question.model';
+import {HttpClientModule} from '@angular/common/http';
+import {CardService} from './services/card.service';
+import {Question} from './models/question.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [
-    HttpClientModule,
-    IonRouterOutlet,
-    IonApp
-  ],
-  standalone: true,
-  providers: [ HttpClientModule],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    imports: [
+        HttpClientModule,
+        IonRouterOutlet,
+        IonApp
+    ],
+    standalone: true,
+    providers: [HttpClientModule],
 
 })
 export class AppComponent {
     questions: Question[] = []; // Initialisiere die Variable mit einem leeren Array
 
-    constructor(private router: Router) {
-  addIcons({personOutline, chevronForward, lockClosedOutline, mailOutline, home, book, addCircle, statsChart, person, arrowBackOutline, logoGoogle});
-        //this.cardService.getQuestions().subscribe(data => {
-            //this.questions = data;
-       // });
-        }
+    constructor(private router: Router, private cardService: CardService) {
+        addIcons({
+            personOutline,
+            chevronForward,
+            lockClosedOutline,
+            mailOutline,
+            home,
+            book,
+            addCircle,
+            statsChart,
+            person,
+            arrowBackOutline,
+            logoGoogle
+        });
+        this.cardService.getQuestions().subscribe(data => {
+            this.questions = data;
+        });
+    }
 }
