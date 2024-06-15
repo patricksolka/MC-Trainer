8aafb71c252d9a17fb4e83fb97995d9a06f6d721
Patrick <patrick.solka@mni.thm.de>
2024-06-15 12:31:57 +0200

    dissolved errors from merge

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {enableProdMode, importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {RouteReuseStrategy, provideRouter} from '@angular/router';
 import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
-import { HttpClientModule } from "@angular/common/http";
+//import { HttpClientModule } from "@angular/common/http";
 import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
 import {environment} from './environments/environment';
