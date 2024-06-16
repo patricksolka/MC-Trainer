3d7581caaeea38a72403604be7b30fa1429cfe55
Patrick <patrick.solka@mni.thm.de>
2024-06-16 14:40:58 +0200

    Displaying Username on Homepage saving userName in localstorage on login and registration removing userName from localstorage on logout and deleteUser

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IonicModule, LoadingController} from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Router, RouterModule} from '@angular/router';
-import { UserService } from 'src/app/services/user.service';
-import { AuthService } from 'src/app/services/auth.service';
+import {UserService} from 'src/app/services/user.service';
+import {AuthService} from 'src/app/services/auth.service';
 import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  standalone: true,
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    standalone: true,
     imports: [IonicModule, CommonModule, RouterModule, FormsModule]
 })
 export class HomeComponent {
-  user: any;
-  constructor(private router: Router, private loadingController: LoadingController, private userService: UserService, private authService: AuthService) {
-    const uid = this.authService.auth.currentUser.uid;
-    this.userService.getUser(uid);
-  }
-  /*async logout() {
-    const loading = await this.loadingController.create({
-      message: 'Logging out...',
-    });
-    await loading.present();
-    await this.authService.logout();
-    loading.dismiss();
-    this.router.navigateByUrl('/login', { replaceUrl: true });
-  }*/
+    public userName: string = localStorage.getItem('userName') || 'User';
+
+    constructor(private router: Router,
+                private loadingController: LoadingController,
+                private userService: UserService,
+                private authService: AuthService)
+    {
+        //this.getUserName();
+    }
+
+    /*async getUserName() {
+        const currentUser = this.authService.auth.currentUser;
+        if (currentUser) {
+            await this.userService.getUser(currentUser.uid).then((userDetails) => {
+                this.userName = localStorage.getItem('userName') || 'User';
+                if (userDetails) {
+                    this.userName = `${userDetails.firstName}`;
+                }
+            });
+        }
+    }*/
+
+    ionViewWillEnter() {
+        this.userName = localStorage.getItem('userName') || 'User';
+        console.log('IonViewWillEnter');
+    }
 }
