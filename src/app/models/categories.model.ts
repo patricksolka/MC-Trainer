93b2c1a0a670050241b61ff461f84e4480bd417d
Rafat Khalid Ahmed Ibrahim <rafat.khalid.ahmed.ibrahim@mni.thm.de>
2024-06-18 14:28:20 +0200

    - by clicking on the module, the user will be redirected to the quiz - Added some code for sorting the modules, but needs to be fixed

--- src/app/models/categories.model.ts
+++ src/app/models/categories.model.ts
@@ -3,4 +3,5 @@ export interface Category {
     id?: string; // Firebase generiert automatisch eine ID
     name: string;
     questionCount?: number; // Anzahl der Fragen
+    lastViewed: number;
 }
