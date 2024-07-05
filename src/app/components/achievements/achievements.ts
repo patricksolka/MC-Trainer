dbd8891db89755c678cb6b64d21fa5339c7b7936
Rafat Khalid Ahmed Ibrahim <rafat.khalid.ahmed.ibrahim@mni.thm.de>
2024-07-05 22:01:52 +0200

    - added Achievement site, which shows the obtained achievements - only 3 achievements so far - added a toast controller to pop up the achieved trophy at its action ! not yet connected to the user account !

--- src/app/components/achievements/achievements.ts
+++ src/app/components/achievements/achievements.ts
@@ -1,9 +1,11 @@
+// src/app/components/achievements/achievements.ts
 import { Stats } from '../../models/stats.model';
 
 export const ACHIEVEMENTS = [
-    { id: 1, name: 'First Step', description: 'First correct answer!', condition:
-            (stats: Stats) => stats.correctAnswers >= 1 },
-    { id: 2, name: 'So far so good', description: 'Five correct answers!', condition:
-            (stats: Stats) => stats.correctAnswers >= 5 },
-    // weitere Achievements hinzufügen
+    { id: 1, name: 'First Step', description: 'First correct answer!', condition: (stats: Stats) => stats.correctAnswers >= 1 },
+    { id: 2, name: 'Quick Learner', description: 'Five correct answers!', condition: (stats: Stats) => stats.correctAnswers >= 5 },
+    { id: 3, name: 'Perseverance', description: 'Ten correct answers!', condition: (stats: Stats) => stats.correctAnswers >= 10 },
+    { id: 4, name: 'First Module Finished', description: 'Completed the first module!', condition: (stats: Stats) => stats.completedQuizzes >= 1 },
+    { id: 5, name: 'Consistent Performer', description: 'Maintained a correct answer rate of 80% or more across 5 quizzes!', condition: (stats: Stats) => stats.correctAnswers / (stats.correctAnswers + stats.incorrectAnswers) >= 0.8 && stats.completedQuizzes >= 5 },
+    { id: 6, name: 'Quiz Master', description: 'Completed 10 quizzes!', condition: (stats: Stats) => stats.completedQuizzes >= 10 },
 ];
