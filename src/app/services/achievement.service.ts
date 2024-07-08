45dce550b0a35c95a094719301b3b6a3f26b6a71
Patrick <patrick.solka@mni.thm.de>
2024-07-08 17:45:36 +0200

    multiple Fixes

--- src/app/services/achievement.service.ts
+++ src/app/services/achievement.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ACHIEVEMENTS } from '../components/achievements/achievements';
-import {doc, Firestore, getDoc, setDoc, collection, getDocs} from "@angular/fire/firestore";
+import {
+    doc,
+    Firestore,
+    getDoc,
+    setDoc,
+    collection,
+    getDocs,
+    CollectionReference, DocumentData
+} from "@angular/fire/firestore";
 import {AuthService} from "./auth.service";
 import {from, Observable} from "rxjs";
 import {map} from "rxjs/operators";
+import {User} from "../models/user.model";
+import {Achievement} from "../models/achievement.model";
 
 @Injectable({
     providedIn: 'root',
@@ -11,7 +21,13 @@ import {map} from "rxjs/operators";
 export class AchievementService {
     private achieved: Set<number> = new Set();
 
-    constructor(private firestore: Firestore, private authService: AuthService) {}
+    public user = User
+
+    userCollectionRef: CollectionReference<DocumentData>
+
+    constructor(private firestore: Firestore, private authService: AuthService) {
+        this.userCollectionRef = collection(firestore, 'users');
+    }
 
     checkAchievements(stats: { completedQuizzes: number; correctAnswers: number, incorrectAnswers: number, totalQuestions: number }) {
         console.log('Checking achievements with stats:', stats);
@@ -36,27 +52,62 @@ export class AchievementService {
     }
 
     loadAchievements(userId: string): Observable<Set<number>> {
-        const achievementsCollection = collection(this.firestore, `users/${userId}/achievements`);
+        const achievementsCollection = collection(this.firestore, `users/${userId}/achievements`).withConverter(this.achievementConverter);
         const achievementsDocs = getDocs(achievementsCollection);
+
         return from(achievementsDocs).pipe(
             map(querySnapshot => {
                 const achievements = new Set<number>();
+                console.log(`Received querySnapshot with ${querySnapshot.size} documents.`);
+
                 querySnapshot.forEach(doc => {
+                    console.log(`Processing document with id: ${doc.id}`);
                     achievements.add(parseInt(doc.id, 10));
                 });
+
                 return achievements;
             })
         );
     }
 
-    getAchievements() {
+
+
+
+
+
+    getAchievements(): Achievement[] {
         return ACHIEVEMENTS.filter(achievement => this.achieved.has(achievement.id));
     }
+
+
+
+
+
     setAchievements(){
         const userId = this.authService.auth.currentUser.uid;
         this.loadAchievements(userId).subscribe(achievements => {
             this.achieved = achievements;
         });
+    }
+
+    private achievementConverter = {
+        toFirestore(achievement: Achievement): any {
+            return {
+                id: achievement.id,
+                name: achievement.name,
+                description: achievement.description,
+                condition: achievement.condition.toString()
+            };
+        },
+        fromFirestore(snapshot: any): Achievement {
+            const data = snapshot.data();
+            return {
+                id: data.id,
+                name: data.name,
+                description: data.description,
+                condition: eval(data.condition)
+            };
+        }
     }
 
     ngOnInit(){
