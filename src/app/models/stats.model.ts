841d6b131f2777f4ff6fdfc348ab57ed62f5ac37
Patrick <patrick.solka@mni.thm.de>
2024-07-08 17:36:00 +0200

    using AchievementModel

--- src/app/models/stats.model.ts
+++ src/app/models/stats.model.ts
@@ -10,21 +10,6 @@ export class Stats {
         this.incorrectAnswers = incorrectAnswers;
     }
 }*/
-/*export class Stats {
-    [key: string]: any;
-
-    //completedQuizzes: number;
-    //totalQuestions: number;
-
-    constructor(
-        //public id: number | any = null,
-        public correctAnswers: number | any = null,
-        public incorrectAnswers: number | any = null,
-        public completedQuizzes: number | any = null,
-    ) {}
-}*/
-
-// models/stats.model.ts
 
 export class Stats {
     [key: string]: any;
@@ -41,38 +26,4 @@ export class Stats {
 }
 
 
-/*export class TotalStats {
-[key: string]: any;
 
-    //completedQuizzes: number;
-    //totalQuestions: number;
-
-    constructor(
-        //public id: number | any = null,
-        /!*public stats: Stats | any = null,*!/
-        public completedQuizzes: number | any = null,
-        //public totalQuestions: number | any = null,
-        public totalIncorrectAnswers: number | any = null,
-        public totalCorrectAnswers: number | any = null
-    ) {
-        /!*this.totalCorrectAnswers = this.totalCorrectAnswers || 0;
-        this.totalIncorrectAnswers = this.totalIncorrectAnswers || 0;
-        this.completedQuizzes = this.completedQuizzes || 0;*!/
-    }
-}*/
-
-/*export class totalStats{
-    stats: Stats;
-    completedQuizzes: number;
-    totalQuestions: number;
-    totalIncorrectAnswers: number;
-    totalCorrectAnswers: number;
-
-    constructor(stats: Stats, completedQuizzes: number, totalQuestions: number, totalIncorrectAnswers: number, totalCorrectAnswers: number) {
-        this.stats = stats;
-        this.completedQuizzes = completedQuizzes;
-        this.totalQuestions = totalQuestions;
-        this.totalIncorrectAnswers = totalIncorrectAnswers;
-        this.totalCorrectAnswers = totalCorrectAnswers;
-    }
-}*/
