cc63131dcdf9a085f8aa7b791324a37cf12c21e2
Dimitrios Paliouras <dimitrios.paliouras@mni.thm.de>
2024-07-08 17:09:25 +0200

    Statistics Page extended after mockup incl. progressbar

--- src/app/components/total-stats/total-stats.component.ts
+++ src/app/components/total-stats/total-stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { TotalStatsService } from '../../services/total-stats.service';
 import { CommonModule } from "@angular/common";
 import {Router, RouterLink} from '@angular/router';
@@ -6,19 +6,20 @@ import {
     AlertController, IonButton, IonButtons,
     IonCard, IonCardContent,
     IonCardHeader,
-    IonCardTitle,
-    IonContent, IonHeader, IonIcon, IonProgressBar, IonTitle, IonToolbar
+    IonCardTitle, IonCol,
+    IonContent, IonGrid, IonHeader, IonIcon, IonItem, IonProgressBar, IonRow, IonTitle, IonToolbar
 } from "@ionic/angular/standalone";
 import {AuthService} from "../../services/auth.service";
 import {CategoryService} from "../../services/category.service";
 import {Category} from "../../models/categories.model";
+import {ProgressBarComponent} from "../progress-bar/progress-bar.component";
 
 @Component({
     selector: 'app-total-stats',
     templateUrl: './total-stats.component.html',
     styleUrls: ['./total-stats.component.scss'],
     standalone: true,
-    imports: [CommonModule, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonProgressBar, IonButtons, IonHeader, IonIcon, IonTitle, IonToolbar, RouterLink]
+    imports: [CommonModule, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonProgressBar, IonButtons, IonHeader, IonIcon, IonTitle, IonToolbar, RouterLink, ProgressBarComponent, IonItem, IonGrid, IonRow, IonCol]
 })
 export class TotalStatsComponent implements OnInit {
     categories: Category[] = [];
@@ -27,6 +28,7 @@ export class TotalStatsComponent implements OnInit {
     completedQuizzes: number = 0;
     percentageCorrectAnswers: number = 0;
     modules: any[] = [];
+    @ViewChild(ProgressBarComponent) progressBarComponent: ProgressBarComponent;
 
     constructor(private totalStatsService: TotalStatsService, private router: Router,
                 private alertController: AlertController, private authService: AuthService, private categoryService: CategoryService) {
@@ -48,5 +50,8 @@ export class TotalStatsComponent implements OnInit {
     }
     ionViewWillEnter() {
        this.loadStats();
+        if (this.progressBarComponent) {
+            this.progressBarComponent.fetchProgress();
+        }
     }
 }
