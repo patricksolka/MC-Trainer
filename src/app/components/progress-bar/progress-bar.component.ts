a09f5f88e1358340a5f35392fb2974576aeb08ad
Patrick <patrick.solka@mni.thm.de>
2024-07-10 01:44:04 +0200

    changed ProgressBar behavior

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -38,9 +38,12 @@ import {CardService} from "../../services/card.service";
 })
 export class ProgressBarComponent {
     public loaded: boolean = false;
+
+    //Progress
     public learnedMinutes: number = 0;
     public totalMinutes: number = 10;
     public progress: number
+    public hasLearningSessions: boolean = false;
 
     constructor(
         private auth: Auth,
@@ -52,14 +55,34 @@ export class ProgressBarComponent {
         const currentUser = this.auth.currentUser;
         if (currentUser) {
             console.log('Current user:', currentUser);
-            this.cardService.getLearningSession(currentUser.uid).subscribe(learningSessions => {
+           /* this.cardService.getLearningSession(currentUser.uid).subscribe(learningSessions => {
                 this.learnedMinutes = Math.round(learningSessions.reduce((total, session) =>
                     total + session['duration'], 0));
                 this.progress = this.calcPercentage();
                 console.log('learnedMinutes', this.learnedMinutes);
                 console.log('progress', this.progress);
                 this.loaded = true;
+            });*/
+            this.cardService.getLearningSession(currentUser.uid).subscribe(learningSessions => {
+                if (learningSessions && learningSessions.length > 0) {
+                    // Calculate learning duration
+                    const totalDuration = learningSessions.reduce((total, session) => total + session['duration'], 0);
+                    this.learnedMinutes = Math.round(totalDuration);
+
+                    // Ensure progress is at least 1 minute if there's any learning session
+                    this.progress = this.learnedMinutes < 1 ? 1 : this.learnedMinutes;
+                } else {
+                    this.learnedMinutes = 0;
+                    this.progress = 0;
+                }
+                console.log('learnedMinutes', this.learnedMinutes);
+                console.log('progress', this.progress);
+                this.loaded = true;
             });
+
+
+
+
         }
     }
     calcPercentage() {
@@ -67,4 +90,6 @@ export class ProgressBarComponent {
         const progressPercentage = (displayedMinutes / this.totalMinutes) * 100;
         return Math.min(progressPercentage, 100);
     }
+
+    protected readonly Math = Math;
 }
