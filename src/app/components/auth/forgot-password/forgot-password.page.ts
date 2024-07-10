f93235923f4f0e1f5b3d603428e631fa447a3612
Patrick <patrick.solka@mni.thm.de>
2024-07-10 17:36:49 +0200

    Reset Password

--- src/app/components/auth/forgot-password/forgot-password.page.ts
+++ src/app/components/auth/forgot-password/forgot-password.page.ts
@@ -1,8 +1,3 @@
-/**
- * @fileoverview Diese Datei enthält die Implementierung der ForgotPasswordPage-Komponente,
- * die es dem Benutzer ermöglicht, das Passwort zurückzusetzen.
- */
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -14,9 +9,12 @@ import {
   IonInput,
   IonItem,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  AlertController,
+  LoadingController
 } from '@ionic/angular/standalone';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 /**
  * @component ForgotPasswordPage
@@ -30,12 +28,57 @@ import {RouterLink} from "@angular/router";
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonInput, IonIcon, IonFab, IonFabButton, RouterLink]
 })
 export class ForgotPasswordPage {
+  email: string = '';
 
   /**
    * @constructor
    * Initialisiert die ForgotPasswordPage-Komponente.
    */
-  constructor() { }
+  constructor(
+      private authService: AuthService,
+      private alertController: AlertController,
+      private loadingController: LoadingController,
+      private router: Router
+  ) {}
 
+  async resetPassword() {
+    if (this.email) {
+      const loading = await this.showLoading('Bitte warten...');
+      try {
+        await this.authService.sendPasswordReset(this.email);
+        await this.showAlert('Passwort zurücksetzen', 'Ein Link zum Zurücksetzen des Passworts' +
+            ' wurde an Ihre E-Mail-Adresse gesendet.');
+      } catch (error) {
+        console.error("Fehler beim Senden des Passwort-Reset-Links:", error);
+      } finally {
+        await loading.dismiss();
+      }
+    } else {
+      console.error("Bitte geben Sie eine E-Mail-Adresse ein.");
+    }
+  }
 
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+          this.router.navigate(['/login']);
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+
+  async showLoading(message: string) {
+    const loading = await this.loadingController.create({
+      message,
+      spinner: 'crescent',
+    });
+    await loading.present();
+    return loading;
+  }
 }
